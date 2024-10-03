bdaf8db0ef02533fc42a88d063a55a9db498210a
adityakumar-14 <hellx1405@gmail.com>
2024-10-03 09:16:01 +0530

    Create callbyvalue.cpp

---
+++ callbyvalue.cpp
@@ -0,0 +1,22 @@
+    #include <iostream>
+    using namespace std;
+    using namespace std;
+    void swap(int x, int y)
+    {
+        int temp;
+        temp=x;
+        x=y;
+        y=temp;
+        
+    
+    }
+    int main(){
+        int a=5, b=2;
+        swap(a,b);
+        cout<<"value of a is;"<<a<<endl;
+        cout<<"value of b is"<<b<<endl;
+    
+    return 0;
+    }
+/*value of a is;5
+value of b is2 */
